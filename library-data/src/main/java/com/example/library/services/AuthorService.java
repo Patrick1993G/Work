a6a5925384535dfc2a0772e7c0465b6a1c0fb895
Patrick1993G <patrick.grech.c10250@mcast.edu.mt>
2020-07-09 10:17:02 +0200

    Refactoring the service interfaces by a Crud service interface

--- library-data/src/main/java/com/example/library/services/AuthorService.java
+++ library-data/src/main/java/com/example/library/services/AuthorService.java
@@ -2,11 +2,7 @@ package com.example.library.services;
 
 import com.example.library.model.Author;
 
-import java.util.Set;
+public interface AuthorService extends CrudService<Author,Long>{
 
-public interface AuthorService {
-    Author findById(Long id);
     Author findByLastName(String lastName);
-    Author save(Author author);
-    Set<Author> findAll();
 }
--- library-data/src/main/java/com/example/library/services/BookService.java
+++ library-data/src/main/java/com/example/library/services/BookService.java
@@ -1,14 +1,9 @@
 package com.example.library.services;
-
-import com.example.library.model.Author;
 import com.example.library.model.Book;
 
 import java.util.Set;
 
-public interface BookService {
-    Book findById(Long id);
+public interface BookService extends CrudService<Book,Long>{
     Book findByIsbn(Long isbn);
     Book findByTitle(String Title);
-    Book save(Book book);
-    Set<Book> findAll();
 }
